27ea21c1f16d3d3eb0e5b7391e42ddd9cd1b8132
Marcelo de Brito Garcia <mbgarcia_pa@yahoo.com.br>
2015-04-12 19:33:15 -0300

    Two calls in the same session

---
+++ src/main/java/br/com/garcia/cache/model/Person.java
@@ -0,0 +1,31 @@
+package br.com.garcia.cache.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Person implements Serializable{
+    private static final long serialVersionUID = -7527047348693267334L;
+
+    @Id
+    private Integer id;
+    
+    @Column
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/br/com/garcia/cache/usage/SessionTest.java
@@ -0,0 +1,27 @@
+package br.com.garcia.cache.usage;
+
+import org.hibernate.Session;
+
+import br.com.garcia.cache.HibernateUtil;
+import br.com.garcia.cache.model.Person;
+
+public class SessionTest {
+
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        
+        session.beginTransaction();
+        
+        Person person = (Person) session.load(Person.class, 1);
+        
+        System.out.println("Person name: " + person.getName());
+        
+        person = (Person) session.load(Person.class, 1);
+        
+        System.out.println("Person name: " + person.getName());
+        
+        session.getTransaction().commit();
+        
+        HibernateUtil.shutdown();
+    }
+}
